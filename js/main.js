81674b9f7cf1a74835611083be1d06f08a5b4a29
Will Richardson <william.hamish@gmail.com>
2023-03-05 07:36:50 +0000

    photo overlay thingie

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const newPreference = (oldpref) => {
 
 const del = id => document.getElementById(id).remove();
 let canvas = null;
-const blurhash = img => {
+const blurhash = (img, hash) => {
   canvas = canvas || document.createElement('canvas');
   let w = 48;
   canvas.width = w;
@@ -29,7 +29,7 @@ const blurhash = img => {
   try {
     const ctx = canvas.getContext("2d");
     const imgdata = ctx.createImageData(w, w);
-    imgdata.data.set(decode(img.dataset.hash, w, w));
+    imgdata.data.set(decode(hash, w, w));
     ctx.putImageData(imgdata, 0, 0);
     let data = canvas.toDataURL();
     img.src = data;
@@ -39,25 +39,47 @@ const blurhash = img => {
 const p = d => d < 10 ? '0' + d : d;
 const sa = (r, el, a, v) => r.querySelector(el).setAttribute(a, v);
 
+const set_metadata = (node, date, f) => {
+  node.querySelector('.date').innerText = date.toLocaleString('default', {year: 'numeric', day: 'numeric', month: 'long'});
+  let l = node.querySelector('.location');
+  l.innerHTML = "&#x1F4CD; " + f.l;
+  l.style.display = f.l.trim().length > 0 ? '' : 'none';
+  let cont = node.querySelector('.content');
+  if (cont) cont.innerText = f.c;
+};
+
+let overlay;
+const show_overlay = info => {
+  let date = new Date(info.d);
+  overlay = overlay || document.getElementById('photo-overlay');
+  let img = overlay.querySelector('img');
+  let src = "/photos/" + date.getFullYear() + "-" + p(date.getMonth() + 1) + '-' + p(date.getDate()) + '.jpeg';
+  img.setAttribute('src', src);
+  set_metadata(overlay, date, info)
+  overlay.style.display = '';
+  overlay.querySelector('date')
+  let cb = () => {
+    img.setAttribute('src', '');
+    overlay.style.display = 'none';
+  };
+  overlay.onclick = cb;
+  overlay.querySelector('button').onclick = cb;
+};
+
 let grid, templ;
 const append_images = images => {
   images.forEach(f => {
     let node = templ.content.cloneNode(true);
+    node.querySelector('.photo-info').addEventListener('click', () => {
+      show_overlay(f);
+    });
     let date = new Date(f.d);
-    sa(node, 'a', 'href', "/" + date.getFullYear() + "/" + p(date.getMonth() + 1) + '/' + p(date.getDate()) + '/post.html');
     sa(node, 'img', 'src', "/thumbnail/" + date.getFullYear() + "-" + p(date.getMonth() + 1) + '-' + p(date.getDate()) + '.jpeg');
     let bh = node.querySelector('img.blurhash');
-    bh.dataset.hash = f.b;
     let img = node.querySelector('img.real');
     img.onload = bh.remove();
-    blurhash(bh);
-    node.querySelector('.date').innerText = date.toLocaleString('default', {year: 'numeric', day: 'numeric', month: 'long'});
-    let l = node.querySelector('.location');
-    if (f.l) {
-      l.innerHTML = "&#x1F4CD; " + f.l;
-    } else {
-      l.remove();
-    }
+    blurhash(bh, f.b);
+    set_metadata(node, date, f)
     grid.appendChild(node);
   });
   if (images.length == 0) {
@@ -90,7 +112,6 @@ addEventListener('load', () => {
 
   window.addEventListener('scroll', scrolled);
 
-  Array.from(document.querySelectorAll('.blurhash')).forEach(blurhash);
   templ = document.getElementById('post-template');
   grid = document.querySelector('.grid')
   if (grid && templ) {
